11b9de49200f9dbd55b4e5d65d55915deea8565f
ParkerEM <eparker00@pm.me>
2022-06-14 17:42:57 -0400

    initial card

--- src/card.js
+++ src/card.js
@@ -4,48 +4,46 @@ class Card extends LitElement {
 	static get properties() {
 		return {
 			endpoint: { type: String },
+			phrase: { type: String },
+			num_Options: {},
+			options: { type: Array },
 		}
 	}
 
 	constructor() {
 		super();
+		this.phrase = 'Do you know what you want?';
 		this.endpoint = '/api/';
+		this.num_Options = 2;
+		this.options = {Yes, No};
 	}
 
 	updated(changedProperties) {
 		changedProperties.forEach((old, propName) => {
-			if (propName === 'city') {
-				this.getWeather(this[propName]);
+			if (propName === 'phrase') {
+				this.getCard(this[propName]);
 			}
 		});
 	}
 
+	static getCard(input,) {
+		this.phrase = input;
+
+	}
+
 	static get styles() {
 		return css`
-			:host {
+			p {
 				display: block;
 			}
-			.overcast {
-				color: white;
-				background-color: #555555;
-			}
-			.mist {
-				color: blue;
-				background-color: black;
-			}
-			.clear.sky {
-				color: black;
-				background-color: blue;
-			}
-			.snow {
-				background-color: orange;
-				color: white;
-			}
 		`;
-	}
+	} 
+
 	render() {
 		return html`
-			
+			<label id="phrase" name="phrase">${this.phrase}</label>
+			<button id="option1">${options[0].value}</button> 
+			<button id="option2">${options.getvalue[1]}</button>
 		`
 	}
 }
