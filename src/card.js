6f82b8ca381e186fef37595fb44755a0be205466
ParkerEM <eparker00@pm.me>
2022-06-18 15:33:43 -0400

    card update

--- src/card.js
+++ src/card.js
@@ -1,7 +1,7 @@
 import { LitElement, html, css } from 'lit';
 import '@lrnwebcomponents/accent-card/accent-card.js';
 
-class Card extends LitElement {
+class XCard extends LitElement {
 	static get properties() {
 		return {
 			endpoint: { type: String },
@@ -10,6 +10,10 @@ class Card extends LitElement {
 			options: { type: Array },
 			card_info: { type: Array },
 		}
+	}
+
+	static get tag() {
+		return 'x-card';
 	}
 
 	constructor() {
@@ -35,20 +39,20 @@ class Card extends LitElement {
 	}
 
 	async loadCard() {
-		await fetch(this.endpoint).then(res => res.json())
-		.then(data => {
-			this.card_info = [];
-			const results = {
-				phrase: data.phrase.value,
-				num_Options: data.num_Options.value,
-				options: data.options,
-			}
-			this.card_info.push(results);
-		});
+		// await fetch(this.endpoint).then(res => res.json())
+		// .then(data => {
+		// 	this.card_info = [];
+		// 	const results = {
+		// 		phrase: data.phrase.value,
+		// 		num_Options: data.num_Options.value,
+		// 		options: data.options,
+		// 	}
+		// 	this.card_info.push(results);
+		// });
 
-		for(var i=0; i<2; i++) {
-			this.card_info.options;
-		};
+		// for(var i=0; i<2; i++) {
+		// 	this.card_info.options;
+		// };
 	}
 
 	static get styles() {
@@ -76,7 +80,7 @@ class Card extends LitElement {
 		<accent-card>
 			<div slot="phrase" name="phrase">${this.phrase} Question</div>
 			${this.options.map(item => html`
-			<button id="option">${item.value} </button>`)}
+			<button name="option">${item.value} </button>`)}
 			<!-- <button id="option1">${options[0].value} Yes</button> 
 			<button id="option2">${options.getvalue[1]} No</button> -->
 		</accent-card>
@@ -84,4 +88,4 @@ class Card extends LitElement {
 	}
 }
 
-customElements.define('custom-card', Card);
+customElements.define(XCard.tag, XCard);
